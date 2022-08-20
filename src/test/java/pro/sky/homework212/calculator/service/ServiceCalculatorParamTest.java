a499eed08b5bf5734246a13632b908a8e2efbf25
nastyafallen <nastyafallen@rambler.ru>
2022-08-20 20:22:57 +0300

    Сделанное задание

--- src/test/java/pro/sky/homework212/calculator/service/ServiceCalculatorParamTest.java
+++ src/test/java/pro/sky/homework212/calculator/service/ServiceCalculatorParamTest.java
@@ -1,5 +1,6 @@
 package pro.sky.homework212.calculator.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,6 +18,28 @@ class ServiceCalculatorParamTest {
     public void shouldReturnSum(float num1, float num2) {
         assertNotNull(out.plus(num1, num2));
         assertEquals(num1 + num2, out.plus(num1, num2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParamsForTests")
+    public void shouldReturnDifference(float num1, float num2) {
+        assertNotNull(out.minus(num1, num2));
+        assertEquals(num1 - num2, out.minus(num1, num2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParamsForTests")
+    public void shouldReturnProduct(float num1, float num2) {
+        assertNotNull(out.multiply(num1, num2));
+        assertEquals(num1 * num2, out.multiply(num1, num2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParamsForTests")
+    public void shouldReturnResultOfDivision(float num1, float num2) {
+        String result = (out.divide(num1, num2));
+        assertNotNull(out.divide(num1, num2));
+        assertFalse(StringUtils.isEmpty(result));
     }
 
 
